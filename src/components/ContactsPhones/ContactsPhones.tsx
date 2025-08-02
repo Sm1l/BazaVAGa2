4e9a68fc64f3c4686413d1a622db87cec512b094
Sm1le <onekonstantin@gmail.com>
2025-08-02 16:53:04 +0300

    delete number alex office

--- src/components/ContactsPhones/ContactsPhones.tsx
+++ src/components/ContactsPhones/ContactsPhones.tsx
@@ -7,7 +7,7 @@ import imgTelegram from "../../assets/images/contacts/telegram.svg";
 import imgTime from "../../assets/images/contacts/time.svg";
 import imgWhatsapp from "../../assets/images/contacts/whatsapp.svg";
 
-import { ALEX_TEL, MAIN_TEL, PIRAT_TEL, YURACH_TEL } from "../../data/constants";
+import { MAIN_TEL, PIRAT_TEL, YURACH_TEL } from "../../data/constants";
 import styles from "./ContactsPhones.module.scss";
 
 interface ContactsPhonesProps {}
@@ -102,31 +102,6 @@ const ContactsPhones: React.FC<ContactsPhonesProps> = () => {
                 </motion.a>
               </div>
               <p className={styles.name}>Александр сервис Пират</p>
-            </div>
-
-            <div className={styles.itemContainer}>
-              <div className={styles.picturesContainer}>
-                <motion.a
-                  initial={{ opacity: 0.5 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 1, delay: 0.4, repeat: Infinity, repeatType: "reverse" }}
-                  href={`tel:${ALEX_TEL}`}
-                  className={styles.imgLink}
-                >
-                  <img src={imgCall} alt="phone" className={styles.img} />
-                </motion.a>
-                <motion.a
-                  initial={{ opacity: 0.5 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ duration: 1, delay: 0.8, repeat: Infinity, repeatType: "reverse" }}
-                  href={`https://wa.me/${ALEX_TEL}`}
-                  className={styles.imgLink}
-                  target="_blank"
-                >
-                  <img src={imgWhatsapp} alt="whatsapp" className={styles.imgW} />
-                </motion.a>
-              </div>
-              <p className={styles.name}>Александр офис</p>
             </div>
           </div>
         </div>
